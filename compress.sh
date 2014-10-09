7c90c5a2e760b6069f1570349dd6bd340fd99d13
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-09 15:07:27 +0200

    pre-scale pieces and boards for normal and HD+ screens

--- compress.sh
+++ compress.sh
@@ -2,13 +2,14 @@
 
 for piece in Basic Glass Metal Wood; do
   orig=Renders/$piece/Scaled
-  dest="$orig/180"
   dimensions="180x230"
-  echo $dest
-  mkdir -p $dest
-  for f in $dir/*.png; do
-    convert -resize $dimensions "$orig/$f" "$dest/${f%.png}.png";
+  echo "$orig/180";
+  cd $orig;
+  mkdir -p 180;
+  for f in *.png; do
+    convert -resize $dimensions "$f" "180/$f";
   done;
+  cd -;
 done
 
 orig=Boards/Scaled
